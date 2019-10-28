17e682f8fb8fee4075322de3f2ea70921d79654d
Panos Anastasiadis <anastapd@gmail.com>
2019-10-28 13:11:51 +0000

    Implemented fetching and adding a log with Redux

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -10,7 +13,12 @@ const AddLogModal = () => {
     if (!message || !tech)
       return M.toast({ html: 'Please a message and tech' });
 
-    console.log(message, tech, attention);
+    const newLog = { message, attention, tech, date: new Date() };
+
+    addLog(newLog);
+
+    M.toast({ html: `Log added by ${tech}` });
+
     setMessage('');
     setTech('');
     setAttention(false);
@@ -83,9 +91,16 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+};
+
 const modalStyle = {
   width: '75%',
   height: '75%'
 };
 
-export default AddLogModal;
+export default connect(
+  null,
+  { addLog }
+)(AddLogModal);
